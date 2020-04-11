96ad3dbe67d1188a7f77b7b883cadedaa6aed6a1
Manish Kumar Singh <manishsinghjss@gmail.com>
2020-04-11 12:06:19 +0530

    Create logging.py

---
+++ logging.py
@@ -0,0 +1,15 @@
+
+import logging
+
+def logger_executor(self):
+    """
+    Creates a rotating log
+    """
+    logger = logging.getLogger(self.logger_name)
+    logger.setLevel(level=self.logger_level)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s")
+    handler = RotatingFileHandler(self.logger_file, maxBytes=10*1024*1024, backupCount=5)
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+  
